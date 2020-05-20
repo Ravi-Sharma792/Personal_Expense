a219c4cd3091bc99b1881f0d81ee1a8a151c6128
Ravi-Sharma792 <ravi14092001@gmail.com>
2020-05-20 21:18:06 +0530

    First Commit

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+import './transaction_item.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    print('build() TransactionList');
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'We are waiting for your first Entry !'
+                    'Tap the + button below to add one',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.gif',
+                      fit: BoxFit.contain,
+                    )),
+              ],
+            );
+          })
+        : ListView(
+            children: transactions
+                .map((tx) => TransactionItem(
+                      key: ValueKey(tx.id),
+                      transaction: tx,
+                      deleteTx: deleteTx,
+                    ))
+                .toList(),
+          );
+  }
+}
